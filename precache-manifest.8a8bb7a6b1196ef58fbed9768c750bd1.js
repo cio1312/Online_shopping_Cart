23f06acc61f38b332c3c59d9d919b1353e66b58f
Valencio ryan Fernandes <>
2022-09-05 05:43:02 +0530

    Updates

---
+++ precache-manifest.8a8bb7a6b1196ef58fbed9768c750bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9518cd74756519cb1f5e94576c2054",
+    "url": "/Online_shopping_Cart/index.html"
+  },
+  {
+    "revision": "99e995e69090ffd96b63",
+    "url": "/Online_shopping_Cart/static/css/main.06ff91d8.chunk.css"
+  },
+  {
+    "revision": "ef6a0cc4f5c1718da4f1",
+    "url": "/Online_shopping_Cart/static/js/2.13e63281.chunk.js"
+  },
+  {
+    "revision": "99e995e69090ffd96b63",
+    "url": "/Online_shopping_Cart/static/js/main.495887e6.chunk.js"
+  },
+  {
+    "revision": "6ba51c518f0d48950f63",
+    "url": "/Online_shopping_Cart/static/js/runtime~main.3bdc1ebe.js"
+  }
+]);
